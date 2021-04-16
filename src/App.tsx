84ed9fb6f8ad031faacd833f507651f5fbaef21a
Andrew Joubert <andrew@siliconrhino.io>
2021-04-16 15:05:51 +0100

    Add material UI
    Change to SCSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
 import React from "react";
-import logo from "./logo512.png";
-import "./App.css";
+import {
+  AppBar,
+  Button,
+  Container,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
+
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+          ></IconButton>
+          <Typography variant="h6">Portal Offline Web App</Typography>
+        </Toolbar>
+      </AppBar>
+    </Container>
   );
 }
 
