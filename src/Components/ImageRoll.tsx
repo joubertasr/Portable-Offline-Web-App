11944b016ef5becdf233fa63bf7aa8b4f7b89711
Andrew Joubert <andrew@siliconrhino.io>
2021-05-14 15:31:19 +0100

    Add download button
    Tidy up icons for removing and downloading images

--- src/Components/ImageRoll.tsx
+++ src/Components/ImageRoll.tsx
@@ -1,24 +1,28 @@
 import React from "react";
 import { Grid, makeStyles } from "@material-ui/core";
 import { IImageItem } from "../Types/ImageStore";
+import RemoveIcon from "@material-ui/icons/RemoveCircleOutline";
+import DownloadIcon from "@material-ui/icons/CloudDownloadRounded";
 const useStyles = makeStyles((theme) => ({
-  paper: {
-    padding: theme.spacing(2),
-    color: theme.palette.text.secondary,
-    display: "flex",
-    flexDirection: "column",
-  },
-  video: {
-    flex: 1,
-    borderStyle: "solid",
-    borderWidth: "0.5rem",
-    borderColor: theme.palette.primary.main,
+  icon: {
+    cursor: "pointer",
+    paddingLeft: theme.spacing(1),
   },
   imagePreview: {
     borderStyle: "solid",
     borderWidth: "0.1rem",
     borderColor: theme.palette.primary.light,
     width: "100%",
+  },
+  itemContainer: {
+    position: "relative",
+  },
+  actionContainer: {
+    position: "absolute",
+    top: "50%",
+    justifyContent: "center",
+    width: "100%",
+    display: "flex",
   },
 }));
 type Props = {
@@ -27,18 +31,37 @@ type Props = {
 };
 export function ImageRoll(props: Props) {
   const styles = useStyles();
-  console.log(styles.video);
   return (
     <Grid container={true}>
       {props.images.map((image, i) => (
-        <Grid key={image.key} item={true} xs={12} sm={6} lg={4} xl={2}>
-          <img
-            src={image.src}
-            className={styles.imagePreview}
-            onClick={() => {
-              props.removeImage(image.key);
-            }}
-          />
+        <Grid
+          key={image.key}
+          item={true}
+          xs={12}
+          sm={6}
+          lg={4}
+          xl={2}
+          className={styles.itemContainer}
+        >
+          <div className={styles.actionContainer}>
+            <RemoveIcon
+              color="primary"
+              className={styles.icon}
+              onClick={() => {
+                props.removeImage(image.key);
+              }}
+            />
+            <a href={`${image.src}`} download={`POWA-image-${image.key}.jpg`}>
+              <DownloadIcon
+                color="primary"
+                className={styles.icon}
+                onClick={() => {
+                  console.log("Download the image");
+                }}
+              />
+            </a>
+          </div>
+          <img src={image.src} className={styles.imagePreview} />
         </Grid>
       ))}
     </Grid>
